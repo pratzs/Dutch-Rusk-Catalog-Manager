8494dd069415e29bb8039f473e3cca9095fec7d6
pratzs <pratzs@live.com>
2026-05-22 10:53:38 +1200

    fix: clear prisma pool exhaustion by optimizing singleton and expanding pool limits

--- app/db.server.js
+++ app/db.server.js
@@ -1,11 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
-if (process.env.NODE_ENV !== "production") {
-  if (!global.prismaGlobal) {
-    global.prismaGlobal = new PrismaClient();
-  }
-}
+const prisma = global.prismaGlobal || new PrismaClient();
 
-const prisma = global.prismaGlobal ?? new PrismaClient();
+if (process.env.NODE_ENV !== "production") {
+  global.prismaGlobal = prisma;
+}
 
 export default prisma;
